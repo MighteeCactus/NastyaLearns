c12dc3a6ef19367ab2f518ea24cdc2b1f0534b60
Alexander <mightee.cactus@gmail.com>
2025-01-08 09:13:09 +0300

    added some ~40 exercises

--- js/workbook.js
+++ js/workbook.js
@@ -25,18 +25,214 @@ export default [
                 'id': 4,
                 'ru': 'Это _лампа_',
                 'en': 'It is _a lamp_'
+            },
+            {
+                id: 0,
+                'ru': 'Я _маленький_.',
+                'en': 'I am _small_.'
+            },
+            {
+                id: 0,
+                'ru': 'Ты _молодой_.',
+                'en': 'You are _young_.'
+            },
+            {
+                id: 0,
+                'ru': 'Он _хороший_.',
+                'en': 'He is _good_.'
+            },
+            {
+                id: 0,
+                'ru': 'Она _маленькая_.',
+                'en': 'She is _little_.'
+            },
+            {
+                id: 0,
+                'ru': 'Мы _друзья_.',
+                'en': 'We are _friends_.'
+            },
+            {
+                id: 0,
+                'ru': 'Они _большие_.',
+                'en': 'They are _big_.'
+            },
+            {
+                id: 0,
+                'ru': 'Это _черное_.',
+                'en': 'This is _black_.'
+            },
+            {
+                id: 0,
+                'ru': 'Это _хорошее_.',
+                'en': 'It is _good_.'
+            },
+            {
+                id: 0,
+                'ru': 'Мне нравится _вода_.',
+                'en': 'I like _water_.'
+            },
+            {
+                id: 0,
+                'ru': 'Ты _остановись_ сейчас.',
+                'en': 'You _stop_ now.'
+            },
+            {
+                id: 0,
+                'ru': 'Мы снова _идем_.',
+                'en': 'We _walk_ again.'
+            },
+            {
+                id: 0,
+                'ru': 'Он _говорит_.',
+                'en': 'He _talks_.'
+            },
+            {
+                id: 0,
+                'ru': 'Она _играет_.',
+                'en': 'She _plays_.'
+            },
+            {
+                id: 0,
+                'ru': 'Я _жду_.',
+                'en': 'I _wait_.'
+            },
+            {
+                id: 0,
+                'ru': 'Они _звонят_.',
+                'en': 'They _call_.'
+            },
+            {
+                id: 0,
+                'ru': 'Мы _работаем_.',
+                'en': 'We _work_.'
+            },
+            {
+                id: 0,
+                'ru': 'Это _новое_.',
+                'en': 'This is _new_.'
+            },
+            {
+                id: 0,
+                'ru': 'Это _большое_.',
+                'en': 'It is _large_.'
+            },
+            {
+                id: 0,
+                'ru': 'Она _улыбается_.',
+                'en': 'She _smiles_.'
+            },
+            {
+                id: 0,
+                'ru': 'Я _останавливаюсь_.',
+                'en': 'I _stop_.'
+            },
+        ]
+    },
+    {
+        'id': 2,
+        'name_ru': 'Простые предложения 2',
+        'name_en': 'Basic sentences 2',
+        'desc_ru': 'Заполни недостающие слова.',
+        'desc_en': 'Fill in missing words',
+        'tasks': [
+            {
+                'id': 1,
+                'ru': 'Я в _комнате_.',
+                'en': 'I am in the _room_.'
+            },
+            {
+                'id': 2,
+                'ru': 'Это _стол_',
+                'en': 'It is _a table_.'
+            },
+            {
+                id: 0,
+                'ru': 'Он _хороший мальчик_.',
+                'en': 'He is a _good boy_.'
+            },
+            {
+                id: 0,
+                'ru': 'Она моя _подруга_.',
+                'en': 'She is my _friend_.'
+            },
+            {
+                id: 0,
+                'ru': 'Мы _идем_ к двери.',
+                'en': 'We _walk_ to the door.'
+            },
+            {
+                id: 0,
+                'ru': 'Ты _молодая девочка_.',
+                'en': 'You are _a young girl_.'
+            },
+            {
+                id: 0,
+                'ru': 'Это _маленькое место_.',
+                'en': 'It is _a small place_.'
+            },
+            {
+                id: 0,
+                'ru': 'Это _мой дом_.',
+                'en': 'This is _my home_.'
+            },
+            {
+                id: 0,
+                'ru': 'Они _мои друзья_.',
+                'en': 'They are _my friends_.'
+            },
+            {
+                id: 0,
+                'ru': 'Мы _играем_ в воде.',
+                'en': 'We _play_ in the water.'
+            },
+            {
+                id: 0,
+                'ru': 'Мир _большой_.',
+                'en': 'The world is _large_.'
+            },
+            {
+                id: 0,
+                'ru': 'Он сейчас _наверху_.',
+                'en': 'He is _up_ now.'
+            },
+            {
+                id: 0,
+                'ru': 'Ты _остановись_ и _спроси_.',
+                'en': 'You _stop_ and _ask_.'
+            },
+            {
+                id: 0,
+                'ru': 'Я снова _говорю_ с тобой.',
+                'en': 'I _talk_ to you again.'
+            },
+            {
+                id: 0,
+                'ru': 'Это _высокое_ место.',
+                'en': 'This is a _high_ place.'
+            },
+            {
+                id: 0,
+                'ru': 'Девочка в _комнате_.',
+                'en': 'The girl is in _the room_.'
+            },
+            {
+                id: 0,
+                'ru': 'Мальчик _дома_.',
+                'en': 'The boy is _at home_.'
+            },
+            {
+                id: 0,
+                'ru': 'Я звоню _своему другу_.',
+                'en': 'I call _my friend_.'
             }
         ]
     },
-
     {
         'id': 999,
         'name_ru': '...',
         'name_en': '...',
         'desc_ru': 'Будем добавлять по мере необходимости.',
         'desc_en': 'We\' add more when we need it',
-        'tasks': [
-
-        ]
+        'tasks': []
     }
 ]
