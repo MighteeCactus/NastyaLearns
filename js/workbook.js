cfeaec947237d3f03c60739defb5d90cb258a1ce
Alexander <mightee.cactus@gmail.com>
2025-02-12 20:48:19 +0300

    added new tasks to train 200 words

--- js/workbook.js
+++ js/workbook.js
@@ -230,6 +230,723 @@ export default [
         ]
     },
     {
+        'id': 3,
+        'name_ru': 'Простые предложения. 200 слов. ',
+        'name_en': 'Basic sentences. 200 words',
+        'desc_ru': 'Заполни недостающие слова.',
+        'desc_en': 'Fill in missing words',
+        'answers_allowed': true,
+        'tasks': [
+            {
+                'id': 0,
+                'ru': 'Я _счастлив_.',
+                'en': 'I am _happy_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _красивая_.',
+                'en': 'She is _beautiful_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он снова будет _играть_.',
+                'en': 'He will _play_ again.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Это мой _дом_.',
+                'en': 'This is my _home_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _друзья_.',
+                'en': 'They are _friends_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Собака_ _черная_.',
+                'en': 'The _dog_ is _black_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы _любим_ _животных_.',
+                'en': 'We _love_ _animals_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я _иду_ к _двери_.',
+                'en': 'I _walk_ to the _door_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _звонит_ своему _брату_.',
+                'en': 'She _calls_ her _brother_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он будет _работать_ _завтра_.',
+                'en': 'He will _work_ _tomorrow_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Солнце_ _жаркое_.',
+                'en': 'The _sun_ is _hot_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Птица_ летает _высоко_.',
+                'en': 'The _bird_ flies _high_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Ты _улыбаешься_ сейчас.',
+                'en': 'You _smile_ now.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _играют_ в _игру_.',
+                'en': 'They _play_ the _game_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы _слушаем_ _музыку_.',
+                'en': 'We _listen_ to _music_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Коробка_ _красная_.',
+                'en': 'The _box_ is _red_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я _люблю_ свою _семью_.',
+                'en': 'I _love_ my _family_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _учится_ в _школе_.',
+                'en': 'She _studies_ in _school_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он _останавливается_ у _стены_.',
+                'en': 'He _stops_ at the _wall_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы едим _еду_ вместе.',
+                'en': 'We eat _food_ together.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Стул_ _маленький_.',
+                'en': 'The _chair_ is _small_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _идут_ в _магазин_.',
+                'en': 'They _walk_ to the _shop_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _улыбается_ _мальчику_.',
+                'en': 'She _smiles_ at the _boy_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Стол_ _большой_.',
+                'en': 'The _table_ is _big_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я _использую_ _телефон_.',
+                'en': 'I _use_ the _phone_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он _разговаривает_ с _доктором_.',
+                'en': 'He _talks_ to the _doctor_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Небо_ _голубое_.',
+                'en': 'The _sky_ is _blue_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы _ждем_ _автобус_.',
+                'en': 'We _wait_ for the _bus_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Комната_ _холодная_.',
+                'en': 'The _room_ is _cold_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _любит_ свою _лошадь_.',
+                'en': 'She _loves_ her _horse_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я _читаю_ _книгу_.',
+                'en': 'I _read_ a _book_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _учатся_ для _теста_.',
+                'en': 'They _study_ for the _test_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Девочка_ _улыбается_.',
+                'en': 'The _girl_ _smiles_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Кофе_ _горячий_.',
+                'en': 'The _coffee_ is _hot_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы едим _еду_.',
+                'en': 'We eat the _food_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он бегает _быстро_.',
+                'en': 'He runs _fast_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Мальчик_ _играет_ в _игру_.',
+                'en': 'The _boy_ _plays_ the _game_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она поет _мягким_ _голосом_.',
+                'en': 'She sings with a _soft_ _voice_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Машина_ _новая_.',
+                'en': 'The _car_ is _new_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _плавают_ в _море_.',
+                'en': 'They _swim_ in the _sea_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я пью _воду_.',
+                'en': 'I drink _water_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Собака_ лает у _двери_.',
+                'en': 'The _dog_ barks at the _door_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы бежим в _магазин_.',
+                'en': 'We run to the _shop_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он _проверяет_ _номер_.',
+                'en': 'He _checks_ the _number_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Учитель_ _задает_ вопрос.',
+                'en': 'The _teacher_ _asks_ a question.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я фотографирую с _камерой_.',
+                'en': 'I take a picture with the _camera_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Фильм_ _хороший_.',
+                'en': 'The _movie_ is _good_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она покупает _чашку_.',
+                'en': 'She buys a _cup_.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Дерево_ высокое.',
+                'en': 'The _tree_ is tall.'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы _любим_ _природу_.',
+                'en': 'We _love_ _nature_.'
+            }
+        ]
+    },
+    {
+        'id': 4,
+        'name_ru': 'Простые вопросы. 200 слов',
+        'name_en': 'Basic questions. 200 words',
+        'desc_ru': 'Заполни недостающие слова.',
+        'desc_en': 'Fill in missing words',
+        'answers_allowed': true,
+        'tasks': [
+            {
+                'id': 0,
+                'ru': 'Я _счастлив_.',
+                'en': 'What makes you _happy_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _красивая_.',
+                'en': 'How is she _beautiful_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он снова будет _играть_.',
+                'en': 'When will he _play_ again?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Это мой _дом_.',
+                'en': 'Where is your _home_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _друзья_.',
+                'en': 'Who are their _friends_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Собака_ _черная_.',
+                'en': 'Which _dog_ is _black_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы _любим_ _животных_.',
+                'en': 'What _animals_ do you _love_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я _иду_ к _двери_.',
+                'en': 'How do you _walk_ to the _door_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _звонит_ своему _брату_.',
+                'en': 'Who does she _call_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он будет _работать_ _завтра_.',
+                'en': 'When will he _work_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Солнце_ _жаркое_.',
+                'en': 'What is _hot_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Птица_ летает _высоко_.',
+                'en': 'How does the _bird_ fly?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Ты _улыбаешься_ сейчас.',
+                'en': 'When do you _smile_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _играют_ в _игру_.',
+                'en': 'Which _game_ do they _play_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы _слушаем_ _музыку_.',
+                'en': 'What _music_ do you _listen_ to?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Коробка_ _красная_.',
+                'en': 'Which _box_ is _red_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я _люблю_ свою _семью_.',
+                'en': 'Who do you _love_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _учится_ в _школе_.',
+                'en': 'Where does she _study_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он _останавливается_ у _стены_.',
+                'en': 'Where does he _stop_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы едим _еду_ вместе.',
+                'en': 'What _food_ do you eat?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Стул_ _маленький_.',
+                'en': 'Which _chair_ is _small_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _идут_ в _магазин_.',
+                'en': 'Where do they _walk_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _улыбается_ _мальчику_.',
+                'en': 'Who does she _smile_ at?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Стол_ _большой_.',
+                'en': 'Which _table_ is _big_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я _использую_ _телефон_.',
+                'en': 'What do you _use_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он _разговаривает_ с _доктором_.',
+                'en': 'Who does he _talk_ to?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Небо_ _голубое_.',
+                'en': 'What color is the _sky_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы _ждем_ _автобус_.',
+                'en': 'What do you _wait_ for?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Комната_ _холодная_.',
+                'en': 'Which _room_ is _cold_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она _любит_ свою _лошадь_.',
+                'en': 'Who does she _love_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я _читаю_ _книгу_.',
+                'en': 'What do you _read_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _учатся_ для _теста_.',
+                'en': 'Which _test_ do they _study_ for?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Девочка_ _улыбается_.',
+                'en': 'Who _smiles_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Кофе_ _горячий_.',
+                'en': 'What is _hot_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы едим _еду_.',
+                'en': 'What do you eat?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он бегает _быстро_.',
+                'en': 'How does he run?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Мальчик_ _играет_ в _игру_.',
+                'en': 'Which _game_ does the _boy_ _play_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она поет _мягким_ _голосом_.',
+                'en': 'How does she sing?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Машина_ _новая_.',
+                'en': 'Which _car_ is _new_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Они _плавают_ в _море_.',
+                'en': 'Where do they _swim_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я пью _воду_.',
+                'en': 'What do you drink?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Собака_ лает у _двери_.',
+                'en': 'Who barks at the _door_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы бежим в _магазин_.',
+                'en': 'Where do you run to?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Он _проверяет_ _номер_.',
+                'en': 'What _number_ does he _check_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Учитель_ _задает_ вопрос.',
+                'en': 'Who _asks_ a question?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Я фотографирую с _камерой_.',
+                'en': 'What do you take with the _camera_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Фильм_ _хороший_.',
+                'en': 'Which _movie_ is _good_?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Она покупает _чашку_.',
+                'en': 'What does she buy?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': '_Дерево_ высокое.',
+                'en': 'Which _tree_ is tall?'
+            },
+
+
+            {
+                'id': 0,
+                'ru': 'Мы _любим_ _природу_.',
+                'en': 'What do you _love_?'
+            },
+
+        ]
+    },
+    {
         'id': 999,
         'name_ru': '...',
         'name_en': '...',
